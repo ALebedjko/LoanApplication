6300131b85f52d4484fe48b384d820140c5f7b3a
arturs <enfocer@gmail.com>
2017-10-26 20:24:22 +0300

    Removed redundant annotation, switched back to h2

--- src/main/java/io/fourfinanceit/HomeworkApplication.java
+++ src/main/java/io/fourfinanceit/HomeworkApplication.java
@@ -2,11 +2,7 @@ package io.fourfinanceit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan(
-        basePackageClasses = {HomeworkApplication.class, org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters.class}
-)
 @SpringBootApplication
 public class HomeworkApplication {
 
